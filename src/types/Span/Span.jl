d291397eae510f3b431a77158c5c899f08f3c677
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-29 17:07:35 -0400

    Update Span.jl

--- src/types/Span/Span.jl
+++ src/types/Span/Span.jl
@@ -2,19 +2,28 @@ struct Span{T} <: AbstractTimeSpan{T}
     value::T
 end
 
-@inline value(x::Span{T}) where T<:IntTimes = x.value
+@inline value(x::Span{T}) where T = x.value
 
-Span(value::T) where T<:IntTimes = Span{T}(value)
+function Span(value::T) where T<:Union{Int16, Int32, UInt16, UInt32, UInt64}
+    try
+        return Span(convert(Int64, value))    
+    catch e
+        throw(e)
+    end
+end
 
-function Span(value::T) where T <:AbstractTime
+function Span(value::T) where T<:Integer
+    try
+        return Span(convert(Int128, value))    
+    catch e
+        throw(e)
+    end
+end
+
+function Span(value::T) where T<:Base.Dates.AbstractTime
     try
          return convert(Span, value)
      catch e
          throw(e)
      end
 end
-
-# use Span{Int64} as the default Span type
-
-Span(x::T) where T = Span{Int64}(x)
-Span(x::T1, y::T2) where T2 where T1 = Span{Int64}(x, y)
