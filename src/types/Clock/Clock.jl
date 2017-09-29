74cde63bded211194b5b019e4dac4b811041cdb9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-29 17:05:40 -0400

    Update Clock.jl

--- src/types/Clock/Clock.jl
+++ src/types/Clock/Clock.jl
@@ -1,10 +1,30 @@
 struct Clock{T} <: AbstractClock{T}
     value::T
+
+    function Clock(value::T) where T<:IntTimes
+        return new{T}(value)
+    end
 end
 
-@inline value(x::Clock{T}) where T<:IntTimes = x.value
+@inline value(x::Clock{T}) where T = x.value
 
-Clock(value::T) where T<:IntTimes = Clock{T}(value)
+Clock(value::T) where T = Clock{T}(value)
+
+function Clock(value::T) where T<:Union{Int16, Int32, UInt16, UInt32, UInt64}
+    try
+        return Clock(convert(Int64, value))    
+    catch e
+        throw(e)
+    end
+end
+
+function Clock(value::T) where T<:Integer
+    try
+        return Clock(convert(Int128, value))    
+    catch e
+        throw(e)
+    end
+end
 
 function Clock(value::T) where T<:Base.Dates.AbstractTime
     try
@@ -13,8 +33,3 @@ function Clock(value::T) where T<:Base.Dates.AbstractTime
          throw(e)
      end
 end
-
-# use Clock{Int64} as the default Clock type
-
-Clock(x::T) where T = Clock{Int64}(x)
-Clock(x::T1, y::T2) where T2 where T1 = Clock{Int64}(x, y)
