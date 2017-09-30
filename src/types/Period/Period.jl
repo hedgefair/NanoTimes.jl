6fad58ba2085b883e081d2494ae5c367bfefd139
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 10:59:38 -0400

    Update Period.jl

--- src/types/Period/Period.jl
+++ src/types/Period/Period.jl
@@ -12,63 +12,31 @@ abstract type MonthBased   <: AbstractTimePeriod end
 abstract type YearBased    <: MonthBased         end
 
 abstract type SubsecondBased  <: AbstractTimePeriod end
-abstract type AttosecondBased <: SubsecondBased     end
-abstract type NanosecondBased <: AttosecondBased    end
+abstract type NanosecondBased <: SubsecondBased     end
 abstract type SecondBased     <: NanosecondBased    end
 abstract type DayBased        <: SecondBased        end
 
-
-#=>
-     For the whole units of time Month and Year with familiar positive integer multiples
-         [month, quarter] as {1, 3} Months and [year, decade] as {1, 10}
-     Each of the YearBased units can be expressed exactly using the MonthBased units.
-     None of the MonthBased units can be expressed exactly using the YearBased units.
-     With abstract subtypes, this simplifies safe dispatch and helps with correctness.
-<=#
-
-abstract type MonthBased   <: AbstractTimePeriod end
-abstract type YearBased    <: MonthBased end
-
-abstract type SubsecondBased  <: AbstractTimePeriod end
-abstract type AttosecondBased <: SubsecondBased     end
-abstract type NanosecondBased <: AttosecondBased    end
-abstract type SecondBased     <: NanosecondBased    end
-abstract type DayBased        <: SecondBased        end
-
-#=
-    struct <symbol> .. end
-    singular form holds a singleton type
-    struct <symbol>s .. end
-    plural form holds a wrapper type
-=#
-
-for (B,S,V) in [
-    (:AttosecondBased, :Attosecond, :Attoseconds),
-    (:AttosecondBased, :Femtosecond, :Femtoseconds),
-    (:AttosecondBased, :Picosecond, :Picoseconds),
-    (:NanosecondBased, :Nanosecond, :Nanoseconds),
-    (:NanosecondBased, :Microsecond, :Microseconds),
-    (:NanosecondBased, :Millisecond, :Milliseconds),
-    (:SecondBased, :Second, :Seconds),
-    (:SecondBased, :Minute, :Minutes),
-    (:SecondBased, :Hour, :Hours),
-    (:MonthBased, :Month, :Months),
-    (:MonthBased, :Quarter, :Quarters),
-    (:YearBased, :Year, :Years),
-    (:YearBased, :Decade, :Decades),
-    (:YearBased, :Century, :Centuries) 
+for (B,V) in [
+    (:NanosecondBased, :Nanoseconds),
+    (:NanosecondBased, :Microseconds),
+    (:NanosecondBased, :Milliseconds),
+    (:SecondBased, :Seconds),
+    (:SecondBased, :Minutes),
+    (:SecondBased, :Hours),
+    (:MonthBased, :Months),
+    (:MonthBased, :Quarters),
+    (:YearBased, :Years),
+    (:YearBased, :Decades),
+    (:YearBased, :Centuries) 
   ]
     @eval begin
         struct $V <: $B
-            value::Int64
+            value::IntSpan
         end
         @inline value(x::$V) = x.value
-
-        $V(x::$V) where T = x
         
         function $V(x::S) where S<:String
-            I = length(x) <= 12 ? Int64 : Int128
-            val = parse(I, x)
+            val = parse(IntSpan, x)
             return $V(val)
         end
     end
