d8e0fdddf9f12bb4bd43c06066b3d737e69acb09
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 09:09:59 -0400

    Update arith.jl

--- src/types/Clock/arith.jl
+++ src/types/Clock/arith.jl
@@ -1,16 +1,14 @@
 import Base: (+), (-)
 
-function (-)(x::Clock{I}, y::Clock{I}) where I<:IntTimes
-    nanos = nanoseconds(x) - nanoseconds(y)
-    return Span{I}(nanos)
+function (-)(x::Clock, y::Clock)
+    nanosecs = nanoseconds(x) - nanoseconds(y)
+    return Span(nanosecs)
 end
-
-(-)(x::Clock{I1}, y::Clock{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
 
 for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week)
     @eval begin
-        (+)(x::Clock{I}, p::$P) where I<:IntTimes = Clock{I}(nanoseconds(x) + nanoseconds(p))
-        (+)(p::$P, x::Clock{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) + nanoseconds(p))
-        (-)(x::Clock{I}, p::$P) where I<:IntTimes = Clock{I}(nanoseconds(x) - nanoseconds(p))
+        (+)(x::Clock{I}, p::$P) = Clock(nanoseconds(x) + nanoseconds(p))
+        (+)(p::$P, x::Clock) = Clock(nanoseconds(x) + nanoseconds(p))
+        (-)(x::Clock, p::$P) = Clock(nanoseconds(x) - nanoseconds(p))
     end
 end
