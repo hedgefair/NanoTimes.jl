0059c1683bb4dca46d32d7e747b653fd0a8c6236
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 08:56:41 -0400

    Update arith.jl

--- src/types/mixed/arith.jl
+++ src/types/mixed/arith.jl
@@ -1,33 +1,27 @@
 import Base: (+), (-)
 
-(+)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) + nanoseconds(y))
-(+)(x::Span{I}, y::Clock{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) + nanoseconds(y))
-(+)(x::Clock{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (+)(promote(x, y)...)
-(+)(x::Span{I1}, y::Clock{I2}) where {I1<:IntTimes, I2<:IntTimes} = (+)(promote(x, y)...)
+(+)(x::Clock, y::Span) = Clock(nanoseconds(x) + nanoseconds(y))
+(+)(x::Span, y::Clock) = Clock(nanoseconds(x) + nanoseconds(y))
 
-(-)(x::Clock{I}, y::Span{I}) where I<:IntTimes = Clock{I}(nanoseconds(x) - nanoseconds(y))
-(-)(x::Clock{I1}, y::Span{I2}) where {I1<:IntTimes, I2<:IntTimes} = (-)(promote(x, y)...)
+(-)(x::Clock, y::Span) = Clock(nanoseconds(x) - nanoseconds(y))
 
-(-)(x::Span{I}, y::Clock{I}) where I<:IntTimes =
-    throw(ErrorException("Span-Clock is invalid;  write Clock-Span."))
-(-)(x::Span{I1}, y::Clock{I2}) where {I1<:IntTimes, I2<:IntTimes} =
+(-)(x::Span, y::Clock) where I<:IntTimes =
     throw(ErrorException("Span-Clock is invalid;  write Clock-Span."))
 
+(-)(x::Clock, y::DateTime) = x - Clock(y)
+(-)(x::DateTime, y::Clock) = Clock(x) - y
+(-)(x::Clock, y::Date) = x - Clock(y)
+(-)(x::Date, y::Clock) = Clock(x) - y
+(-)(x::Clock, y::Time24) = x - Span(y)
 
-(-)(x::Clock{I}, y::DateTime) where I<:IntTimes = x - Clock{I}(y)
-(-)(x::DateTime, y::Clock{I}) where I<:IntTimes = Clock{I}(x) - y
-(-)(x::Clock{I}, y::Date) where I<:IntTimes = x - Clock{I}(y)
-(-)(x::Date, y::Clock{I}) where I<:IntTimes = Clock{I}(x) - y
-(-)(x::Clock{I}, y::Time24) where I<:IntTimes = x - Span{I}(y)
+(-)(x::DateTime, y::Span) = Clock(x) - y
+(-)(x::Date, y::Span) = Clock(x) - y
+(-)(x::Span, y::Time24) = x - Span(y)
 
-(-)(x::DateTime, y::Span{I}) where I<:IntTimes = Clock{I}(x) - y
-(-)(x::Date, y::Span{I}) where I<:IntTimes = Clock{I}(x) - y
-(-)(x::Span{I}, y::Time24) where I<:IntTimes = x - Span{I}(y)
+(+)(x::DateTime, y::Span{I}) = Clock(x) + y
+(+)(x::Date, y::Span) = Clock(x) + y
+(+)(x::Time24, y::Span) = Span(x) + y
 
-(+)(x::DateTime, y::Span{I}) where I<:IntTimes = Clock{I}(x) + y
-(+)(x::Date, y::Span{I}) where I<:IntTimes = Clock{I}(x) + y
-(+)(x::Time24, y::Span{I}) where I<:IntTimes = Span{I}(x) + y
-
-(+)(x::Span{I}, y::DateTime) where I<:IntTimes = Clock{I}(y) + x
-(+)(x::Span{I}, y::Date) where I<:IntTimes = Clock{I}(y) + x
-(+)(x::Span{I}, y::Time24) where I<:IntTimes = x + Span{I}(y)
+(+)(x::Span, y::DateTime) = Clock(y) + x
+(+)(x::Span, y::Date) = Clock(y) + x
+(+)(x::Span, y::Time24) = x + Span(y)
