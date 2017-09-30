8578beece598009b039aee99dd43a8973142115a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 10:13:58 -0400

    Update arith.jl

--- src/types/Clock/arith.jl
+++ src/types/Clock/arith.jl
@@ -5,8 +5,11 @@ function (-)(x::Clock, y::Clock)
     return Span(nanosecs)
 end
 
-for P in (:Nanosecond, :Microsecond, :Millisecond, :Second, :Minute, :Hour, :Day, :Week,
-          :Nanoseconds, :Microseconds, :Milliseconds, :Seconds, :Minutes, :Hours, :Days, :Weeks)
+
+for P in [:Nanosecond, :Microsecond, :Millisecond, 
+          :Second, :Minute, :Hour, :Day, :Week,
+		  :Nanoseconds, :Microseconds, :Milliseconds, 
+          :Seconds, :Minutes, :Hours]
     @eval begin
         (+)(x::Clock, p::$P) = Clock(nanoseconds(x) + nanoseconds(p))
         (+)(p::$P, x::Clock) = Clock(nanoseconds(x) + nanoseconds(p))
