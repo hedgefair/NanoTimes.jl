88d6822966bfa71372cfbf08ca1ba02ae7aa5e2a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 11:36:24 -0400

    Create arith.jl

---
+++ src/types/Period/arith.jl
@@ -0,0 +1,26 @@
+Base.:(+)(x::T, y::T) where T<:SecondBased =
+    (T)(value(x) + value(y))
+Base.:(-)(x::T, y::T) where T<:SecondBased =
+    (T)(value(x) - value(y))
+Base.:(+)(x::T, y::T) where T<:NanosecondBased =
+    (T)(value(x) + value(y))
+Base.:(-)(x::T, y::T) where T<:NanosecondBased =
+    (T)(value(x) - value(y))
+Base.:(+)(x::T, y::T) where T<:MonthBased =
+    (T)(value(x) + value(y))
+Base.:(-)(x::T, y::T) where T<:MonthBased =
+    (T)(value(x) - value(y))
+
+Base.:(+)(x::T1, y::T2) where T1<:SecondBased where T2<:SecondBased =
+    (+)(promote(x, y)...)
+Base.:(-)(x::T1, y::T2) where T1<:SecondBased where T2<:SecondBased =
+    (-)(promote(x, y)...)
+Base.:(+)(x::T1, y::T2) where T1<:NanosecondBased where T2<:NanosecondBased =
+    (+)(promote(x, y)...)
+Base.:(-)(x::T1, y::T2) where T1<:NanosecondBased where T2<:NanosecondBased =
+    (-)(promote(x, y)...)
+Base.:(+)(x::T1, y::T2) where T1<:MonthBased where T2<:MonthBased =
+    (+)(promote(x, y)...)
+Base.:(-)(x::T1, y::T2) where T1<:MonthBased where T2<:MonthBased =
+    (-)(promote(x, y)...)
+	
