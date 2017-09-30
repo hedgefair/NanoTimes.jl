82e0b0756f6af4ee91316323440864cd9a41c661
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 10:23:37 -0400

    Update string.jl

--- src/types/Span/string.jl
+++ src/types/Span/string.jl
@@ -17,28 +17,28 @@ function Base.string(x::Span)
     subsecs = string(millis, micros, nanos)
 
     subgranular = length(subsecs) > 0
-    if scs === zero(T)
+    if scs === zero(IntTime)
         secs = subgranular ? string("00.", subsecs) : ""
     else
         secs = subgranular ? string(twodigits(scs), DEC_PT, subsecs) : twodigits(scs)
     end
 
     subgranular = length(secs) > 0
-    if mns === zero(T)
+    if mns === zero(IntTime)
         mins = subgranular ? string("00", HMS_SEP, secs) : ""
     else
         mins = subgranular ? string(twodigits(mns), HMS_SEP, secs) : twodigits(mns)
     end
 
     subgranular = length(mins) > 0
-    if hrs === zero(T)
+    if hrs === zero(IntTime)
         hours = subgranular ? string("00", HMS_SEP, mins) : ""
     else
         hours = subgranular ? string(twodigits(hrs), HMS_SEP, mins) : twodigits(hrs)
     end
 
     subgranular = length(hours) > 0
-    if dys === zero(T)
+    if dys === zero(IntTime)
         days = subgranular ? hours : ""
     else
         days = subgranular ? string(dys, DYHR_SEP, hours) : string(dys, DYHR_SEP)
