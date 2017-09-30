a6bab4d836f5fe19c2b6d6271901bf8e021e25c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 09:35:54 -0400

    Update arith.jl

--- src/types/mixed/arith.jl
+++ src/types/mixed/arith.jl
@@ -18,7 +18,7 @@ import Base: (+), (-)
 (-)(x::Date, y::Span) = Clock(x) - y
 (-)(x::Span, y::Time24) = x - Span(y)
 
-(+)(x::DateTime, y::Span{I}) = Clock(x) + y
+(+)(x::DateTime, y::Span) = Clock(x) + y
 (+)(x::Date, y::Span) = Clock(x) + y
 (+)(x::Time24, y::Span) = Span(x) + y
 
