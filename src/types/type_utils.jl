d2caae5e6d32a0c5d66c2aa74f98ac8ea9529880
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 08:40:20 -0400

    Update type_utils.jl

--- src/types/type_utils.jl
+++ src/types/type_utils.jl
@@ -1,6 +1,1 @@
-Base.sum(x::CompoundPeriod) = sum(typeof(x.periods[end]).(x.periods))
-
-@inline nanosecs(x::NanoType{I}) where I<:IntTimes = nanoseconds(x)
-@inline nanosecs(x::Span{I}) where I<:IntTimes = nanoseconds(x)
-@inline nanosecs(x::P) where P<:Period = nanosecond(Nanosecond(x))
-@inline nanosecs(x::P) where P<:CompoundPeriod = nanosecs(sum(x))
+Base.sum(x::Base.Dates.CompoundPeriod) = sum(typeof(x.periods[end]).(x.periods))
