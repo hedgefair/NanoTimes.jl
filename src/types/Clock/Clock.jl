1b83bc7dd6965235012f619e79625a66d6c3e609
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-30 14:31:58 -0400

    Update Clock.jl

--- src/types/Clock/Clock.jl
+++ src/types/Clock/Clock.jl
@@ -4,26 +4,11 @@ end
 
 @inline value(x::Clock) = x.value
 
-function Clock(value::T) where T<:Union{Int16, Int32, UInt16, UInt32, UInt64}
-    try
-        return Clock(convert(IntTime, value))    
-    catch e
-        throw(e)
-    end
-end
-
 function Clock(value::T) where T<:Integer
-    try
-        return Clock(convert(IntTime, value))    
-    catch e
-        throw(e)
-    end
+    val = IntTime(value)
+	return Clock(val)    
 end
 
 function Clock(value::T) where T<:Base.Dates.AbstractTime
-    try
-         return convert(Clock, value)
-     catch e
-         throw(e)
-     end
+     return convert(Clock, value)
 end
