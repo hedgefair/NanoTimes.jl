07befc498d4f965b04826a448a444d5d9ec1d9b7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-02 00:50:47 -0400

    Update preference.jl

--- src/support/preference.jl
+++ src/support/preference.jl
@@ -6,32 +6,41 @@
 	
 	To use local mean times only
 	   NANOTIMES_ARE_LMT = true
-	
-	To have both available
-       NANOTIMES_ARE_BOTH = true
 
-	You may set your choice prior to reading this file,
+     You may set your choice prior to reading this file,
 	or you may set your choice in the ENV:
 	
 	   NANOTIMES_ARE_LMT = true
 	   using NanoTimes
 	
-       # in local environment or in .juliarc file
-	   ENV[ "NANOTIMES_ARE_BOTH" ] = true
-       using NanoTimes      	   
 <=#
 
-NanoTimeFrame = 0 # default is TIMES_ARE_UMT
+const NANOTIME_IS_UTM =	(
 
-if ((@isdefined TIMES_ARE_UMT) && TIMES_ARE_UMT) ||
-   (haskey(ENV,"TIMES_ARE_UMT") && ENV["TIMES_ARE_UMT"])
-   NanoTimeFrame = 0
-elseif ((@isdefined TIMES_ARE_LMT) && TIMES_ARE_LMT) ||
-   (haskey(ENV,"TIMES_ARE_LMT") && ENV["TIMES_ARE_LMT"])
-   NanoTimeFrame = 1
-elseif ((@isdefined TIMES_ARE_BOTH) && TIMES_ARE_BOTH) ||
-   (haskey(ENV,"TIMES_ARE_BOTH") && ENV["TIMES_ARE_BOTH"])
-   NanoTimeFrame = 2
-end  
+ if VERSION > v"0.7.0-"
 
-const NANOTIME_FRAME = NanoTimeFrame; NanoTimeFrame = nothing
+  const NANOTIME_IS_UTM =	
+    if ((@isdefined TIMES_ARE_UMT) && TIMES_ARE_UMT) ||
+       (haskey(ENV,"TIMES_ARE_LUMT") && ENV["TIMES_ARE_UMT"])
+         NanoTimeFrame = true
+    elseif ((@isdefined TIMES_ARE_LMT) && TIMES_ARE_LMT) ||
+       (haskey(ENV,"TIMES_ARE_LMT") && ENV["TIMES_ARE_LMT"])
+         NanoTimeFrame = false
+    else
+        true
+    end
+	
+ else
+
+    if ((isdefined(:TIMES_ARE_UMT)) && TIMES_ARE_UMT) ||
+       (haskey(ENV,"TIMES_ARE_UMT") && ENV["TIMES_ARE_UMT"])
+         NanoTimeFrame = true
+    elseif ((isdefined(:TIMES_ARE_LMT)) && TIMES_ARE_LMT) ||
+       (haskey(ENV,"TIMES_ARE_LMT") && ENV["TIMES_ARE_LMT"])
+         NanoTimeFrame = false
+    else
+        true
+    end
+
+ end		
+)
