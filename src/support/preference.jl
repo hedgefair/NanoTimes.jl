5167b3b92322fe76ca0c243f7a5f0080a64a25cf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-02 01:31:05 -0400

    Update preference.jl

--- src/support/preference.jl
+++ src/support/preference.jl
@@ -1,57 +1,20 @@
-
+#=>
     This package has a few configurations available.
 
-    universal mean time (UTM)
-       timekeeping that follows the practice  scientific consensus timekeeping [almost] anchored on the Greenwich Meridan
+    Universal Mean Time (UMT)
+       Timekeeping that follows civil convention
+       and wobbles with astronometric wave energy.
+       Universal Times are times given an observer
+       [almost] anchored on the Greenwich Meridan.
+       Universal Mean Time is without leap seconds.
 
-(leapsecond unaware of leapseconds)
-lazy mean solar time at the 
-       time() is as now(Base.Dates.UTC), shown as UT
-       date() is as Date(time()), with respect to UT
-       timeofday() is clock time at the Greenwhich meridian
+     Local Mean Time (LMT)
+       Timekeeping that follows civil convention
+       and wobbles with astronometric wave energy.
+       Local Times are times given for an observer
+       at the present location.  Local Mean Times
+       are given with respect to local time zone.
+       This is the time given by a very accurate
+       wall clock. Leap seconds are ingnored.
 
-
-	To use universal mean times directly
-UMT   
-	   NANOTIMES_ARE_UMT = true           <=== the default
-	
-	To use local mean times only
-	   NANOTIMES_ARE_LMT = true
-
-     You may set your choice prior to reading this file,
-	or you may set your choice in the ENV:
-	
-	   NANOTIMES_ARE_LMT = true
-	   using NanoTimes
-	
 <=#
-
-const NANOTIME_IS_UTM =	(
-
- if VERSION >= v"0.7.0-DEV.1816"
-
-  const NANOTIME_IS_UTM =	
-    if ((@isdefined TIMES_ARE_UMT) && TIMES_ARE_UMT) ||
-       (haskey(ENV,"TIMES_ARE_UMT") && ENV["TIMES_ARE_UMT"])
-         NanoTimeFrame = true
-    elseif ((@isdefined TIMES_ARE_LMT) && TIMES_ARE_LMT) ||
-       (haskey(ENV,"TIMES_ARE_LMT") && ENV["TIMES_ARE_LMT"])
-         NanoTimeFrame = false
-    else
-        true
-    end
-	
- else
-
-    if ((isdefined(:TIMES_ARE_UMT)) && TIMES_ARE_UMT) ||
-       (haskey(ENV,"TIMES_ARE_UMT") && ENV["TIMES_ARE_UMT"])
-         NanoTimeFrame = true
-    elseif ((isdefined(:TIMES_ARE_LMT)) && TIMES_ARE_LMT) ||
-       (haskey(ENV,"TIMES_ARE_LMT") && ENV["TIMES_ARE_LMT"])
-         NanoTimeFrame = false
-    else
-        true
-    end
-
- end		
-)
